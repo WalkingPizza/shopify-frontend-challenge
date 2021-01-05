8b11c8135a77110562230754ef2dd11f6dbd2229
Kevin Antonio Rateni Iatauro <34224657+WalkingPizza@users.noreply.github.com>
2021-01-05 22:15:49 +0100

    Fixed homepage

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SharedList from './SharedList/SharedList';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/shopify-frontend-challenge">
     <AppProvider i18n={enTranslations}>
       <Route exact path="/" component={App} />
       <Route path="/list/:nominations" component={SharedList} />
